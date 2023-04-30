cd3e5451924bbccd0a00f0cfb828b268ad511b7b
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-30 03:17:35 +0200

    chore: init

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 <a href="https://marketplace.visualstudio.com/items?itemName=antfu.vscode-server-controller" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/antfu.vscode-server-controller.svg?color=eee&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
 
+> You may NOT need to install the extension directly. This is mainly for tooling integrations.
+
+Start a local server for controlling the VS Code instance.
+
+Port of the server will be stored under `.vscode/.server-controller-port.log` in the root of the current workspace.
+
+## Endpoints
+
+### `GET /open`
+
+Open a file or folder in the current VS Code instance.
+
+Required query parameters:
+
+- `path`: the path to open
+
 ## Sponsors
 
 <p align="center">
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,71 @@
-import { window } from 'vscode'
+import { createServer } from 'node:http'
+import fs from 'node:fs/promises'
+import { join } from 'node:path'
+import type { ExtensionContext } from 'vscode'
+import { Position, Range, Selection, window, workspace } from 'vscode'
+import { createApp, eventHandler, getQuery, toNodeListener } from 'h3'
+import { getPort } from 'get-port-please'
+import { version } from '../package.json'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export async function activate(ctx: ExtensionContext) {
+  const logger = window.createOutputChannel('VS Code Server Controller')
+
+  const root = workspace.workspaceFolders?.[0].uri.fsPath
+  if (!root) {
+    logger.appendLine('No workspace folder found')
+    return
+  }
+
+  const app = createApp()
+  app.use('/open', eventHandler((ctx) => {
+    const query = getQuery(ctx)
+    if (!query.path)
+      return { status: 'error', message: 'No path provided' }
+
+    const [file, line = 1, column = 1] = query.path?.toString().split(':')
+
+    logger.appendLine(`Open ${file}:${line}:${column}`)
+
+    workspace.openTextDocument(file)
+      .then(doc => window.showTextDocument(doc))
+      .then((editor) => {
+        const pos = new Position(+line, +column)
+        editor.revealRange(new Range(pos, pos))
+        editor.selection = new Selection(pos, pos)
+      })
+
+    return { status: 'ok' }
+  }))
+
+  const server = createServer(toNodeListener(app))
+
+  ctx.subscriptions.push({
+    async dispose() {
+      server.close()
+    },
+  })
+
+  const port = await getPort({ port: 13452, portRange: [9000, 15000] })
+
+  server.listen(port, () => {
+    logger.appendLine(`Server listening on port ${port}`)
+  })
+
+  const dir = join(root, '.vscode')
+  const filepath = join(dir, '.server-controller-port.log')
+
+  await fs.mkdir(dir, { recursive: true })
+  await fs.writeFile(filepath, JSON.stringify({
+    port,
+    time: Date.now(),
+    version,
+  }, null, 2), 'utf-8')
+
+  ctx.subscriptions.push({
+    async dispose() {
+      await fs.rm(filepath)
+    },
+  })
 }
 
 export function deactivate() {
